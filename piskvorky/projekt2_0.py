3ea672890edcb704401882089bc73a88abd45e86
MichalSnop <michalsnopko84@gmail.com>
2023-11-18 23:51:14 +0100

    Použit while cyklus

--- piskvorky/projekt2_0.py
+++ piskvorky/projekt2_0.py
@@ -22,3 +22,51 @@ def check_winner(board, mark):
     if all([board[i][i] == mark for i in range(3)]) or all([board[i][2 - i] == mark for i in range(3)]):
         return True
     return False
+def tic_tac_toe():
+    print("Welcome to Tic Tac Toe")
+    print("===========================================")
+    print("GAME RULES:")
+    print("Each player can place one mark (or stone)")
+    print("per turn on the 3x3 grid. The WINNER is")
+    print("who succeeds in placing three of their")
+    print("marks in a:")
+    print("* horizontal,")
+    print("* vertical or")
+    print("* diagonal row")
+    print("===========================================")
+    print("Let's start the game")
+    print("-------------------------------------------")
+
+    board = [[" " for _ in range(3)] for _ in range(3)]
+    display_board(board)
+
+    current_mark = "X"
+    
+    for _ in range(9):
+        while True:
+            try:
+                move = int(input(f"Player {current_mark} | Please enter your move number (1-9): "))
+                row = (move - 1) // 3
+                col = (move - 1) % 3
+
+                if board[row][col] != " ":
+                    print("This position is already taken. Try again.")
+                    continue
+
+                board[row][col] = current_mark
+                break
+            except (ValueError, IndexError):
+                print("Invalid input. Please enter a number between 1 and 9.")
+
+        display_board(board)
+
+        if check_winner(board, current_mark):
+            print(f"Congratulations, the player {current_mark} WON!")
+            return
+
+        current_mark = "O" if current_mark == "X" else "X"
+
+    print("It's a tie!")
+
+# Spuštění hry
+tic_tac_toe()
