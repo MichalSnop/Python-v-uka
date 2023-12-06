d7caf7c3dc929ab8a938c91468ed523ecceabc35
MichalSnop <michalsnopko84@gmail.com>
2023-12-06 08:35:24 +0100

    Importovány requests a bs4 - BeautifulSoup

---
+++ nove prostredi/moje_prvni_prostredi/webscr.py
@@ -0,0 +1,8 @@
+"""
+projekt_3.py: třetí projekt do Engeto Online Python Akademie
+author: Michal Snopko
+email: michalsnopko84@gmail.com
+discord: michalsn.
+"""
+import requests
+from bs4 import BeautifulSoup
