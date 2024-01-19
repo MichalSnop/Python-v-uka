c204955b32440e86ae3ad0f6fad7ca8e8d1a6fbc
MichalSnop <michalsnopko84@gmail.com>
2024-01-19 19:34:57 +0100

    Vytvorena funkce save_to_csv

--- nove prostredi/prostredi_pro_webscr/myenv/webscr4.py
+++ nove prostredi/prostredi_pro_webscr/myenv/webscr4.py
@@ -12,3 +12,14 @@ import sys
 
 MAIN_URL = "https://volby.cz/pls/ps2017nss/"
 url, OUTPUT_FILE = "", ""
+
+#FIRST PAGE TABLE DATA (cities etc.)
+CODE = "t{}sa1 t{}sb1"
+CITY = "t{}sa1 t{}sb2"
+LINK = "t{}sa2"
+
+#SECOND PAGE TABLE DATA (candidate parties etc.)
+CAND_PARTY = "t{}sa1 t{}sb2"
+NUMBER = "t{}sa2 t{}sb3"
+
+FIRST_ROW = ["Code", "City", "Registered", "Envelopes", "Votes"]
