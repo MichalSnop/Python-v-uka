02c65bee6f642284fe5b12822429607343f97197
MichalSnop <michalsnopko84@gmail.com>
2024-01-19 23:52:19 +0100

    funkce get_sum_numbers

--- nove prostredi/prostredi_pro_webscr/myenv/webscr4.py
+++ nove prostredi/prostredi_pro_webscr/myenv/webscr4.py
@@ -23,3 +23,16 @@ CAND_PARTY = "t{}sa1 t{}sb2"
 NUMBER = "t{}sa2 t{}sb3"
 
 FIRST_ROW = ["Code", "City", "Registered", "Envelopes", "Votes"]
+
+#Tato funkce ukládá data do CSV souboru. Přijímá data o kandidujících stranách a další relevantní informace pro jednotlivé obce. Vytváří CSV soubor se získanými daty.
+def save_to_csv(*data):
+    print(f"Ukladam do souboru {OUTPUT_FILE}...")
+    with open(OUTPUT_FILE, mode="w", newline="", encoding="Windows-1250") as file:
+        f_writer = csv.writer(file)
+        f_writer.writerow(FIRST_ROW + cand_parties)
+        for index, (code, city, envelope_info, party_number) in enumerate(
+                zip(codes, cities, all_header_data, party_numbers)):
+            volic = envelope_info[0]
+            envelope = envelope_info[1]
+            vote = envelope_info[2]
+            f_writer.writerow([code, city, volic, envelope, vote] + party_number)
