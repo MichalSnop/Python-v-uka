9f5ae47cee39f463d1f453d62497d84df7052402
MichalSnop <michalsnopko84@gmail.com>
2024-02-28 05:20:35 +0100

    ...

--- nove prostredi/prostredi_pro_webscr/myenv/webscr4_2.py
+++ nove prostredi/prostredi_pro_webscr/myenv/webscr4_2.py
@@ -1,14 +1,14 @@
 """
-webscr4.py: third project to Engeto Online Python Academy
+webscr4_2.py: third project to Engeto Online Python Academy
 author: Michal Snopko
 email: michalsnopko84@gmail.com
 discord: michalsn.
 """
 
-import requests
-from bs4 import BeautifulSoup
 import csv
 import sys
+import requests
+from bs4 import BeautifulSoup
 
 MAIN_URL = "https://volby.cz/pls/ps2017nss/"
 url, OUTPUT_FILE = "", ""
@@ -30,11 +30,8 @@ def save_to_csv(*data):
     with open(OUTPUT_FILE, mode="w", newline="", encoding="Windows-1250") as file:
         f_writer = csv.writer(file)
         f_writer.writerow(FIRST_ROW + cand_parties)
-        for index, (code, city, envelope_info, party_number) in enumerate(
-                zip(codes, cities, all_header_data, party_numbers)):
-            volic = envelope_info[0]
-            envelope = envelope_info[1]
-            vote = envelope_info[2]
+        for code, city, envelope_info, party_number in zip(codes, cities, all_header_data, party_numbers):
+            volic, envelope, vote = envelope_info
             f_writer.writerow([code, city, volic, envelope, vote] + party_number)
 #Tato funkce provádí součet čísel pro jednotlivé strany a obce, které mají více než jeden volební obvod.
 def get_sum_numbers(middle_numbers):
@@ -78,33 +75,27 @@ def get_party_numbers(soup_2):
 
 #Tato funkce spočítá informace jako počet registrovaných voličů, vydaných obálek a platných hlasů pro všechny volební obvody daného města nebo obce.
 def sum_header_data(middle_links):
+    volici_in_list, provide_envelopes, valid_votes = 0, 0, 0
     for web in middle_links:
-        volici_in_list, provide_envelopes, valid_votes = 0, 0, 0
         for web_page in web:
             url_arg = MAIN_URL + web_page
             soup_3 = get_html(url_arg)
             for tb in soup_3.find_all("table", {"id": "ps311_6_t1"}):
-                vol_sez, vyd_ob, pl_hl = '', '', ''
                 tds = tb.find_all("td")
+                vol_sez, vyd_ob, pl_hl = '', '', ''
                 for char in tds[1].text:
                     if char.isnumeric() or char == ",":
                         vol_sez += str(char)
-                    else:
-                        continue
                 for char in tds[3].text:
                     if char.isnumeric() or char == ",":
                         vyd_ob += str(char)
-                    else:
-                        continue
                 for char in tds[4].text:
                     if char.isnumeric() or char == ",":
                         pl_hl += str(char)
-                    else:
-                        continue
                 volici_in_list += int(vol_sez.replace(",", "."))
                 provide_envelopes += int(vyd_ob.replace(",", "."))
                 valid_votes += int(pl_hl.replace(",", "."))
-        return [str(volici_in_list), str(provide_envelopes), str(valid_votes)]
+    return [str(volici_in_list), str(provide_envelopes), str(valid_votes)]
 
 #Tato funkce získává informace o kandidujících stranách.
 def get_cand_parties(soup_2):
@@ -128,27 +119,21 @@ def get_middle_links(soup_2):
     
 #Tato funkce získává informace jako počet registrovaných voličů, vydaných obálek a platných hlasů pro všechny volební obvody daného města nebo obce bez jejich součtu.
 def get_all_header_data(soup_2):
+    header_data = []
+    volici_in_list, provide_envelopes, valid_votes = '', '', ''
     for tb in soup_2.find_all("table", {"id": "ps311_t1"}):
-        header_data = []
-        volici_in_list, provide_envelopes, valid_votes = '', '', ''
         tds = tb.find_all("td")
         for char in tds[3].text:
             if char.isnumeric() or char == ",":
                 volici_in_list += str(char)
-            else:
-                continue
         for char in tds[4].text:
             if char.isnumeric() or char == ",":
                 provide_envelopes += str(char)
-            else:
-                continue
         for char in tds[7].text:
             if char.isnumeric() or char == ",":
                 valid_votes += str(char)
-            else:
-                continue
-        return [volici_in_list, provide_envelopes, valid_votes]
-    
+    return [volici_in_list, provide_envelopes, valid_votes]
+
 #Tato funkce získává data z druhé úrovně stránek volebních obvodů, včetně informací o kandidujících stranách a počtu hlasů.
 def get_second_data(links):
     all_header_data, middle_links, party_numbers = [], [], []
@@ -170,16 +155,19 @@ def get_second_data(links):
 def get_first_columns(soup):
     tables = soup.find_all("div", {"class": "t3"})
     codes, cities, links = [], [], []
+    
     for index, tr in enumerate(tables):
-        codes += [td.text for td in tr.find_all("td", {"headers": CODE.format(index + 1, index + 1)}) if
-                  not td.text == "-"]
-    for index, tr in enumerate(tables):
-        cities += [td.text for td in tr.find_all("td", {"headers": CITY.format(index + 1, index + 1)}) if
-                   not td.text == "-"]
-    for index, tr in enumerate(tables):
-        for td in tr.find_all("td", {"headers": LINK.format(index + 1)}):
+        code_cells = tr.find_all("td", {"headers": CODE.format(index + 1, index + 1)})
+        city_cells = tr.find_all("td", {"headers": CITY.format(index + 1, index + 1)})
+        link_cells = tr.find_all("td", {"headers": LINK.format(index + 1)})
+        
+        codes += [td.text for td in code_cells if not td.text == "-"]
+        cities += [td.text for td in city_cells if not td.text == "-"]
+        
+        for td in link_cells:
             for link in td.find_all("a"):
                 links.append(link.get("href"))
+    
     return codes, cities, links
 
 #Tato funkce získává HTML kód stránky pomocí knihovny requests a BeautifulSoup.
@@ -188,13 +176,13 @@ def get_html(url):
     html = r.text
     return BeautifulSoup(html, "html.parser")
 
-try:
-    url, OUTPUT_FILE = sys.argv[1:]
-    if not ".cz" in url and not ".csv" in OUTPUT_FILE:
-        print("Spatne poradi argumentu. UKONCUJI election-scraper")
-        exit()
-except ValueError as e:
-    print(f"{e.__class__.__name__}: Nespravny pocet zadanych argumentu. UKONCUJI election-scraper")
+if len(sys.argv) != 3:
+    print("Nespravny pocet zadanych argumentu. UKONCUJI election-scraper")
+    exit()
+
+url, OUTPUT_FILE = sys.argv[1:]
+if not ".cz" in url and not ".csv" in OUTPUT_FILE:
+    print("Spatne poradi argumentu. UKONCUJI election-scraper")
     exit()
 
 print(f"STAHUJI DATA Z VYBRANEHO URL: {url}")
